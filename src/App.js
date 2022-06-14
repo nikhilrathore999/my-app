eaadf974802972d483679d9ae42f1421379b94d7
Nikhil Rathore (NCS) <nikhil.rathore@in.ncs-i.com>
2022-06-14 22:26:48 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1,
+      show: true,
+      max: 5,
+      min: 0
+    };
+  }
+
+  IncrementItem = () => {
+      this.setState(prevState => {
+        if(prevState.quantity < 9) {
+          return {
+            quantity: prevState.quantity + 1
+          }
+        } else {
+          return null;
+        }
+      });
+  }
+  DecreaseItem = () => {
+    this.setState(prevState => {
+      if(prevState.quantity > 0) {
+        return {
+          quantity: prevState.quantity - 1
+        }
+      } else {
+        return null;
+      }
+    });
+  }
+  ToggleClick = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+  handleChange = (event) => {
+    this.setState({quantity: event.target.value});
+  }
+
+  render() {
+
+    return ( <div>
+      <h1>Increment and Decrement</h1>
+      <button onClick={this.IncrementItem}>+</button>
+      <input className="inputne" value={this.state.quantity} onChange={this.handleChange}/>
+      <button onClick = {this.DecreaseItem}>- </button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
